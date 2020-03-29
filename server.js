b391c1d20846b986d8139925bf152361dfbf9df0
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-03-29 17:18:55 +0300

    Testing git commands

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("API Running");
 });
-
+//Testing git commands
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
