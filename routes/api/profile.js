0a5c7a2558373e3cfa27aa7efa3de081d6afd45c
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-04-02 22:58:45 +0300

    models/Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,13 @@
 // FOR = anything tı do with profiles.Adding,fetching,updating them etc.
 const express = require("express");
 let router = express.Router();
+const auth = require("../../middleware/auth");
+//If we want to protect the route we just get it and add it as a second parameter
 
-//@route   GET(type) api/profile(endpoint)
-//@desc    Test route(what this route does)
-//@access  Public(public or private)
-router.get("/", (req, res) => {
+//@route   GET(type) api/profile/me(endpoint)
+//@desc    Get current users profile
+//@access  Private
+router.get("/", auth, (req, res) => {
   res.send("Profile route");
 });
 
