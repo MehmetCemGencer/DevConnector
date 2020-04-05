3fa73551cd0a6c043fe75c3a96530de677dc6f4e
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-04-06 00:41:28 +0300

    Post Like & Unlike Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -112,4 +112,70 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+//@route   PUT api/posts/like/:id
+//@desc    Like a post
+//@access  Private
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Check if the post has already been liked by same user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      //Because this is a filter function it will return an array and
+      //if user already liked it it will return an array with 1
+      //if user did not liked it it wont return anything
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    //We will see in the front end why he returned post.likes
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//@route   PUT api/posts/unlike/:id
+//@desc    Unlike a post
+//@access  Private
+
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Check if the post has already been liked by same user
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      //Because this is a filter function it will return an array and
+      //if user already liked it it will return an array with 1
+      //if user did not liked it it wont return 0
+      //and if not liked you cant unlike it
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    //Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString()) //it will return objectId that why toString
+      .indexOf(req.user.id);
+    //this will get the correct like to remove
+
+    //take out of array
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+
+    //We will see in the front end why he returned post.likes
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
