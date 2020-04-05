604928e8efa1dad03b0b2555777036ec2f078494
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-04-05 20:17:17 +0300

    Github get route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const auth = require("../../middleware/auth");
 //If we want to protect the route we just get it and add it as a second parameter
 
 const { check, validationResult } = require("express-validator");
+const config = require("config");
+const axios = require("axios");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -349,6 +351,31 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
+  }
+});
+
+//@route   Get api/profile/github/:username
+//@desc    Get user repos from Github
+//@access  Public
+
+router.get("/github/:username", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      //5 repo per page and ascended order(recent to old)
+      //it will give 5 repos in the json format if you want more change the number
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `token ${config.get("githubToken")}`,
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+    //if you dont put return it will give an error "headers already sent"
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server errors");
   }
 });
 
