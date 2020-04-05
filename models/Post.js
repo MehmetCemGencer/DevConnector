40aeb3517eb66435260bcb9aa474fe73b125ee4e
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-04-05 21:24:48 +0300

    Post model

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  //post will be connected to a user
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    //this is the username not post name
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      //this way a person can like once
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        //this is the username not post name
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
