fe44a42acba9f62330d88979144f9dbb6296b0ee
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-04-13 22:52:39 +0300

    Login Form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  //Destructring formData !!!!! use this
+  const { email, password } = formData;
+  //because of this destructring we can say value={name}
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    return setFormData({ ...formData, [name]: value });
+  }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Success");
+  }
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            value={password}
+            onChange={handleChange}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -15,13 +16,13 @@ const Register = () => {
     const { name, value } = event.target;
     return setFormData({ ...formData, [name]: value });
   }
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     if (password !== password2) {
       //this is for now change it later
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      console.log("Success");
     }
   }
   return (
@@ -78,7 +79,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
