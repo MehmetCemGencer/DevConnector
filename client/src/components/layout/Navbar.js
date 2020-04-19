a869080cf8478c6046d092c875669c44a29390f6
MehmetCemGencer <cem.gencer.35@gmail.com>
2020-04-19 17:23:57 +0300

    Logout & Navbar Links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+//Connect to redux
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
 
-const Navbar = () => {
+//loading needed because want to make sure loading is done before putting the links
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i classname='fas fa-sign-out-alt'></i>
+          {"  "}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='#!'>Developers</a> {/*#! goes nowhere*/}
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,19 +38,20 @@ const Navbar = () => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <a href='profiles.html'>Developers</a>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired, //function
+  auth: PropTypes.object.isRequired, //state
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth, //reducers/auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from "../actions/types";
 //reason for doing this(creating initialState) is not to put
 //all parameters to the function params place
@@ -50,6 +51,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
